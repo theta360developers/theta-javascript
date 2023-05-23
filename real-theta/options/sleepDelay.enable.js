630a7524a211b8177005ebd34641f38c69ba6814
erikrodriquez <erikbolt11@gmail.com>
2023-05-23 11:54:28 -0700

    Added sleepDelay commands, removed all comments

--- real-theta/options/sleepDelay.enable.js
+++ real-theta/options/sleepDelay.enable.js
@@ -1,12 +1,12 @@
 const body = {'name': 'camera.setOptions',
               "parameters": {
                 "options": {
-                    "offDelay": 600
+                    "sleepDelay": 60
         }   
     }
 }
 
-const response = await fetch('https://fake-theta.vercel.app/osc/commands/execute', 
+const response = await fetch('http://192.168.1.1/osc/commands/execute', 
 	{method: 'POST', 
 	body: JSON.stringify(body),
 	headers: {'Content-Type': 'application/json'}
